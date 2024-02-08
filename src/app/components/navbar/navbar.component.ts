5e947a1bf5855b4042e68c00da615c5df7f77c7f
emirmorgan <53477261+emirmorgan@users.noreply.github.com>
2024-02-09 02:50:51 +0300

    feat: Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll() {
+    const scrollOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    this.isSticky = scrollOffset > 80;
+  }
+}
