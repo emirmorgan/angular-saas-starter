a5dbe6b07099ff1fe01784aa9651cdb6e552d9d0
emirmorgan <53477261+emirmorgan@users.noreply.github.com>
2024-02-13 02:39:05 +0300

    feat: Section / Features

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { NavbarComponent } from '../../components/home/navbar/navbar.component';
 import { HeroComponent } from '../../components/home/hero/hero.component';
+import { FeaturesComponent } from '../../components/home/features/features.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, FeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass',
 })
