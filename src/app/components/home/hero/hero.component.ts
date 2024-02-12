b067777e1257e75bb563a7a4d3d9f8895ee3ac7e
emirmorgan <53477261+emirmorgan@users.noreply.github.com>
2024-02-12 22:55:52 +0300

    feat: Section / Hero - List the companies

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+import { Company } from '../../../models/company';
 
 @Component({
   selector: 'section-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.sass',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  companies: Company[] = [
+    {
+      name: 'Nike',
+      src: '../../../../assets/logos/nike.svg',
+    },
+    {
+      name: 'Stripe',
+      src: '../../../../assets/logos/stripe.svg',
+    },
+    {
+      name: 'Disney',
+      src: '../../../../assets/logos/disney.svg',
+    },
+    {
+      name: 'Coca-Cola',
+      src: '../../../../assets/logos/coca-cola.svg',
+    },
+    {
+      name: 'Nasa',
+      src: '../../../../assets/logos/nasa.svg',
+    },
+  ];
+}
